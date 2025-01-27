3b215225d0844a18f0612a4999a61db146139cd9
Felipe Augusto <felipementel@hotmail.com>
2025-01-27 17:15:45 -0300

    feat: add exemples and update packages

--- src/PoC.TempTables.EF/Program.cs
+++ src/PoC.TempTables.EF/Program.cs
@@ -76,23 +76,47 @@ namespace PoC.TempTables.EF
             //SELECT, INSERT, UPDATE, DELETE
 
 
-              // Pessoas
-            var user = new Microsoft.Data.SqlClient.SqlParameter("Nome", "CANAL DEPLOY");
-            var pessoa1 = dbContext.Pessoas
-                .FromSql($"EXECUTE dbo.GetMostPopularBlogsByName @filterByUser={user}")
-                .ToList();
 
 
-            var pessoa2 = dbContext.Pessoas
-                .FromSql<Pessoa>($"SELECT TOP 1 * FROM Pessoas ORDER BY CreatedAt DESC")
-                .ToList();
+            /*O EF Core suporta vários operadores de consulta de tabela temporal:
 
-            var pessoa3 = await dbContext.Pessoas.FindAsync(pessoa2[0].Id);
+            TemporalAsOf: Retorna linhas que estavam ativas (atuais) no horário UTC fornecido. Esta é uma única linha da tabela de histórico para uma determinada chave primária.
+            TemporalAll: Retorna todas as linhas nos dados históricos. Normalmente, são muitas linhas da tabela de histórico para uma determinada chave primária.
+            TemporalFromTo: Retorna todas as linhas que estavam ativas entre dois horários UTC fornecidos. Podem ser muitas linhas da tabela de histórico para uma determinada chave primária.
+            TemporalBetween: O mesmo que TemporalFromTo, exceto que as linhas incluídas se tornaram ativas no limite superior.
+            TemporalContainedIn: : Retorna todas as linhas que começaram a ser ativas e terminaram a ser ativas entre dois horários UTC fornecidos. Podem ser muitas linhas da tabela de histórico para uma determinada chave primária.
+            */
 
-            // update pessoa nome
-            var item5 = dbContext.Pessoas.Where(p => p.Nome.Any()).ExecuteUpdate(s => s.SetProperty(p => p.Nome, "NOVO NOME"));
+            dbContext.Pessoas.TemporalAll().Single(product => product.Nome.Contains("CANAL"));
 
-            var item6 = dbContext.Pessoas.Where(p => p.Nome.StartsWith("NO")).ExecuteUpdate(s => s.SetProperty(p => p.Nome, "NOVO NOME 2"));
+
+
+            //// Pessoas
+            //var user = new Microsoft.Data.SqlClient.SqlParameter("Nome", "CANAL DEPLOY");
+            //var pessoa1 = dbContext.Pessoas
+            //    .FromSql($"EXECUTE dbo.GetMostPopularBlogsByName @filterByUser={user}")
+            //    .ToList();
+
+
+            //var Pessoa1 = dbContext.Pessoas.Single(product => product.Nome.Contains("CANAL"));
+
+            //var Pessoa2 = dbContext.Pessoas.TemporalAll().Single(product => product.Nome.Contains("CANAL"));
+
+
+
+
+
+
+            //var pessoa2 = dbContext.Pessoas
+            //    .FromSql<Pessoa>($"SELECT TOP 1 * FROM Pessoas ORDER BY CreatedAt DESC")
+            //    .ToList();
+
+            //var pessoa3 = await dbContext.Pessoas.FindAsync(pessoa2[0].Id);
+
+            //// update pessoa nome
+            //var item5 = dbContext.Pessoas.Where(p => p.Nome.Any()).ExecuteUpdate(s => s.SetProperty(p => p.Nome, "NOVO NOME"));
+
+            //var item6 = dbContext.Pessoas.Where(p => p.Nome.StartsWith("NO")).ExecuteUpdate(s => s.SetProperty(p => p.Nome, "NOVO NOME 2"));
         }
     }
 }
